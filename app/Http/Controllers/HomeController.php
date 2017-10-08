e15dfe5a825f7479a5a28334014c76d7306677f2
Ved Patel <vedmpatel@gmail.com>
2017-10-08 22:33:55 +0530

    bug fixed - redundant names in chat

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,9 +30,10 @@ class HomeController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        $pc = Chat::whereIn('c_id',PersonalChat::where('user_id_1',Auth::user()->id)->orWhere('user_id_2',Auth::user()->id)->pluck('id')->toArray())->get();
-        $gc = Chat::whereIn('c_id',GroupMember::where('user_id',Auth::user()->id)->pluck('group_chat_id')->toArray())->get();
+        $pc = Chat::where('type','personal')->whereIn('c_id',PersonalChat::where('user_id_1',Auth::user()->id)->orWhere('user_id_2',Auth::user()->id)->pluck('id')->toArray())->get();
+        $gc = Chat::where('type','group')->whereIn('c_id',GroupMember::where('user_id',Auth::user()->id)->pluck('group_chat_id')->toArray())->get();
 
+        return ($pc->merge($gc))->sortBy('created_at');
         return view('home', [
             'users' => User::get()->all(),
             'chats' => ($pc->merge($gc))->sortBy('created_at')
